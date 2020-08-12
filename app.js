3d1ea63cc74e59cc025e4fd3bac23573f4e55749
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-12 11:41:40 -0400

    added mongo url and added conditional to run if it's a str

--- app.js
+++ app.js
@@ -2,15 +2,30 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
-
+//setting up 
 let app = express();
 const port = process.env.PORT || 3000
+const URI = process.env.MONGO_DB
+if (typeof(URI) === 'string') {
+    const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true}
+    mongoose.connect(URI, mongoOptions, (error) => {
+        if (error) {
+            console.error(`\nError Connecting to MongoDB: ${error.message || err} \n`);
+        } else {
+            console.log('Server connected to DB');
+            
+        }
+    })
+} else {
+    console.error('Invalid connection to database!');
+    
+}
+//importing routers
+const homeRouter = require('./routes/homeRouter')
 
-
-
-
+app.use('/', homeRouter);
 
 
 app.listen(port, () => {
-    `Now successfully listening to ${port}.`
+    console.log(`Now successfully listening to ${port}.`)
 })
