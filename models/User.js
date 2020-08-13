f29e03f1c4c5bef7677d81cc8718dabdf1980ca3
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-13 19:36:37 -0400

    exported the model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,34 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 33,
+        unique: true,
+    },
+    password: {
+        type: String,
+        minlength: 7,
+        required: true
+    },
+    email: {
+        type: String,
+        minlength: 6,
+        maxlength: 200,
+        unique: true,
+        required: true
+    },
+    emailValidated: { 
+        type:Boolean,
+        default: false,
+    }
 
-});
+})
+
+const UserModel = mongoose.model('user', UserSchema);
+
+module.exports = UserModel;
+//this allows you to interface with your database in any other file in your program
+
