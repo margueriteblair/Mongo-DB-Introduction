d0d6f61bfb176444b2a61559d7d190508dca3a20
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-14 06:15:28 -0400

    register page

--- public/register.js
+++ public/register.js
@@ -1,51 +1,83 @@
+
+
 window.onload = () => {
+    //create a form and append to a div
+    //create blank elements 
+    //set properties and event listeners
+    //append to the dom
     const header = document.createElement('h1')
-    const form = document.createElement('form')
-    const email = document.createElement('input');
-    const password = document.createElement('input')
-    const signInButton = document.createElement('button');
-
-    document.body.appendChild(header);
-    document.body.appendChild(form);
-    form.appendChild(email);
-    form.appendChild(password);
-    document.body.appendChild(signInButton);
-
-
-    header.id = 'header'
-    header.innerText = "Sign In"
+    const mainDiv = document.createElement('div');
+    const form = document.createElement('form');
+    const emailInput = document.createElement('input')
+    const passwordInput = document.createElement('input')
+    const passwordConfirm = document.createElement('input')
+    const userNameInput = document.createElement('input')
+    const submitButton = document.createElement('button')
+    
+    emailInput.placeholder = 'Email';
+    emailInput.type = 'email';
+    emailInput.id = 'email'
+    emailInput.name = 'email'
+    passwordInput.placeholder = 'Password';
+    passwordInput.type = 'password'
+    passwordInput.name = 'pswd1'
+    passwordInput.id = 'pswd1'
+    passwordConfirm.placeholder = 'Confirm Password';
+    passwordConfirm.type = 'password';
+    passwordConfirm.id = 'pswd2';
+    passwordConfirm.name = 'pswd2'
+    userNameInput.placeholder = 'Username';
+    userNameInput.type = 'text'
+    userNameInput.name = 'username';
+    userNameInput.id = 'username'
+    submitButton.innerText = 'Submit';
+    header.innerText = 'Register Today!';
     form.id = 'form';
-    email.id = 'email'
-    email.name = 'email'
-    email.type = 'email'
-    email.placeholder = 'Email'
-    password.id = 'pswd';
-    password.type = 'password'
-    password.name = 'pass';
-    password.placeholder = 'Password'
-    signInButton.innerText = 'Sign In'
-
-    signInButton.onclick = logData;
-
-
-}
-
-function logData() {
-    console.log('Data was submitted')
-    let formElems = document.getElementById('form');
-    const endpoint = location.origin + '/user/login';
-    const reqBody = {}; //setting an empty object for the request body
-    for (const input of formElems) {
-        reqBody[input.name] = input.value;
+    
+    
+    document.body.appendChild(header)
+    document.body.appendChild(mainDiv);
+    mainDiv.appendChild(form);
+    mainDiv.appendChild(submitButton)
+    form.appendChild(userNameInput)
+    form.appendChild(emailInput)
+    form.appendChild(passwordInput)
+    form.appendChild(passwordConfirm)
+    
+    let inputs = document.querySelectorAll('input');
+    for (const input of inputs) {
+        input.classList.add('input');
     }
-    const xhr = new XMLHttpRequest();
-    xhr.open('PATCH', endpoint);
-    xhr.onload = () => {
-        const response = JSON.parse(xhr.responseText);
-        console.log(response);
-        //xhr onload responds on the frontend
-        //the response text is the frontend response
+    
+    
+    submitButton.addEventListener('mouseup', () => {
+        console.log('You submitted your information!')
+        for (const child of form.children) {
+            console.log(child)
+        }
+        for (const input of inputs) {
+            console.log(input.value)
+        }
+    submitButton.onclick = submitReg;
+        //console.log(form.children)
+    })
+    
     }
-    xhr.setRequestHeader('Content-Type', 'application/json')
-    xhr.send(JSON.stringify(reqBody));
-}
+    
+    function submitReg() {
+        const formElem = document.getElementById('form');
+        const reqBody = {};
+        for (const input of formElem) {
+            reqBody[input.name] = input.value
+        }
+        console.log(reqBody)
+        const endpoint = location.origin + '/user/post/new';
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', endpoint)
+        xhr.onload = () => {
+            const res = JSON.parse(xhr.responseText)
+            console.log(res)
+        }
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.send(JSON.stringify(reqBody))
+    }
