2bae6bc02ce7ad5e9c6b431b5c890cee5f0bb18f
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-15 09:09:51 -0400

    home routes updated

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -2,9 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/',
+     (req, res) => {
     res.sendfile(process.cwd() + '/public/index.html')
     //console.log(req.body)
-})
+});
+
+router.get('/login',
+    (req, res) => {
+        const filePath = process.cwd() + '/public/login.html'
+        res.sendFile(filePath);
+    })
 
 module.exports = router
