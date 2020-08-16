0f4b823fa2b3c31be4d6e021a04adebcf6a57092
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-16 09:25:44 -0400

    comments

--- app.js
+++ app.js
@@ -9,8 +9,13 @@ const firstMid = require('./middleware/firstMiddleware')
 //setting up 
 let app = express();
 app.use(express.static('public'))
+
+//env vars
 const port = process.env.PORT || 3000
 const URI = process.env.MONGO_DB
+// mongo.connect(URI, options, error) where uri is that link to the db and the cluster, options is an object
+//error is the third thing and it's optional
+//app.use expects a middleware function
 if (typeof(URI) === 'string') {
     const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true}
     mongoose.connect(URI, mongoOptions, (error) => {
