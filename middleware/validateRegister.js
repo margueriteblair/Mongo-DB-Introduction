3234c4ecac13cafd7e737a5154766b615de04a97
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-01 20:38:04 -0400

    tiered validation for all of the request body

--- middleware/validateRegister.js
+++ middleware/validateRegister.js
@@ -3,24 +3,33 @@ const User = require('../models/User')
 const validator = require('validator').default;
 
 
-
 module.exports = async (req, res, next) => {
     try {
-        const {email, username, password} = req.body //this is a concept called object destructuring
-        //checks if the data exists with if statements
+        const {email, username, password} = req.body 
         const validationErrors = [];
-        const emailExist = await User.findOne({email: email}) !== null;
-        const usernameExist = await User.findOne({username: username}) !== null;
-            if (email !== undefined && !validator.isEmail(email)) {
-                validationErrors.push({key: 'email', error: 'Must be valid email address'})
-            }
-            if (emailExist) {
-                validationErrors.push({key: 'email', error: 'email is in use'})
-            }
-            if (usernameExist) {
-                validationErrors.push({key: 'username', error: 'username is in use'});
-            }
-            if (password.length < 7) validationErrors.push({key: 'password', error: 'password didnt meet requirements' })
+
+        if (email === undefined || email.trim().length === 0) {
+            validationErrors.push({key: 'email', error: "Email Required"})
+        } else if (email !== undefined && !validator.isEmail(email)) {
+            validationErrors.push({key: 'email', error: 'Must be a valid email address'});
+        } else {
+            const emailExist = await User.findOne({email: email}) !== null;
+            if (emailExist) validationErrors.push({key: 'email', error: 'email is in use'})
+        }
+
+        if (username === undefined || username.trim().length === 0) {
+            validationErrors.push({key: 'username', error: 'Username required'})
+        } else {
+            const usernameExist = await User.findOne({username: username}) !== null;
+            if (usernameExist) validationErrors.push({key: "username", error: "username in use"});
+        }
+
+        if (password === undefined || password.trim().length === 0) {
+            validationErrors.push({key: "password", error: "password required"})
+        } else if (password !== undefined && password.length < 7) {
+            validationErrors.push({key: 'password', error: 'password didnt meet requirements' })
+        }
+        
             if (validationErrors.length > 0) {
                 res.status(400).json({
                     "validation errors" : validationErrors
@@ -28,7 +37,6 @@ module.exports = async (req, res, next) => {
             } else {
                 next();
             }
-        //if this array has more than 0 elements, respond with the array of errors
     } catch (error) {
         res.status(500).json({message: error.message || "Unknown Error"})
     }
